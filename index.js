b2ce66726c97634cc9b8445bb9e9ef5f0f48dbd3
David Mugnai <dvd@gnx.it>
2023-07-04 10:24:43 +0200

    Don't force the non-code import to be external
    An external resource has a special meaning that cannot be enforced by
    a plugin

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+const path = require("path");
 const { readFileSync } = require('fs');
-
 const { transform } = require('@svgr/core');
 
 module.exports = (options = {}) => ({
@@ -11,27 +11,34 @@ module.exports = (options = {}) => ({
                 case 'require-call':
                 case 'dynamic-import':
                 case 'require-resolve':
-                    return;
-                default:
+                    // an import of a SVG from the code
                     return {
-                        external: true,
+                        path: path.resolve(args.resolveDir, args.path),
+                        namespace: "svgr",
                     };
+                default:
+                    // everything else, most probably an url from a CSS
+                    return;
             }
         });
 
-        build.onLoad({ filter: /\.svg$/ }, async (args) => {
+        build.onLoad({ filter: /\.svg$/, namespace: "svgr" }, async (args) => {
             const svg = readFileSync(args.path, { encoding: 'utf8' });
             const contents = await transform(svg, { ...options }, { filePath: args.path });
 
             if (args.suffix === '?url') {
                 return {
                     contents: args.path,
+                    resolveDir: path.dirname(args.path),
                     loader: 'text',
                 };
             }
 
             return {
                 contents,
+                // the resolveDir is required in order to delegate the load to
+                // a standard loader because we used a custom namespace
+                resolveDir: path.dirname(args.path),
                 loader: options.typescript ? 'tsx' : 'jsx',
             };
         });
